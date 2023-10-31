0ca3e923ed5c4928cb535778e07d353ce55d428a
Abhijit Naik <naikabhiit@gmail.com>
2023-11-01 00:03:31 +0530

    Added Retrofit dependency; Re-organized and labelled all external dependencies
    Signed-off-by: Abhijit Naik <naikabhiit@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,20 +50,34 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    // Tooling - Preview
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+
+    // Compose UI
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
+
+    // Compose Jetpack Core
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.core:core-ktx:1.12.0")
+
+    // Compose UI - Material 3
     implementation("androidx.compose.material3:material3")
+
+    // Unit & UI Tests
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Compose - BOM(Bill of Materials)
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
