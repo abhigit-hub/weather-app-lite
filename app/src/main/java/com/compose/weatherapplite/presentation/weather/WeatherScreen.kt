3d53bdce73f52489d4325a9eee72280d99b3ea37
Abhijit Naik <naikabhiit@gmail.com>
2023-11-01 21:10:50 +0530

    Enabled edge to edge in the activity to span across entire display
    Signed-off-by: Abhijit Naik <naikabhiit@gmail.com>

--- app/src/main/java/com/compose/weatherapplite/presentation/weather/WeatherScreen.kt
+++ app/src/main/java/com/compose/weatherapplite/presentation/weather/WeatherScreen.kt
@@ -1,8 +1,11 @@
 package com.compose.weatherapplite.presentation.weather
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 
@@ -16,7 +19,8 @@ fun WeatherScreen(
         Text(
             text = state.toString(),
             fontSize = 32.sp,
-            color = Color.White
+            color = Color.White,
+            modifier = Modifier.padding(horizontal = 30.dp, vertical = 100.dp)
         )
     }
 }
--- app/src/main/java/com/compose/weatherapplite/ui/theme/Theme.kt
+++ app/src/main/java/com/compose/weatherapplite/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.compose.weatherapplite.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,11 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 val LightColors = lightColorScheme(
     primary = md_theme_light_primary,
@@ -82,6 +87,15 @@ fun WeatherAppLiteTheme(
         } else {
             if (darkTheme) DarkColors else LightColors
         }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = androidx.compose.ui.graphics.Color.Transparent.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
+    }
 
     MaterialTheme(
         colorScheme = colorScheme,
